5c7efc9c55f05278916b5d173e8a0ee3ccee1fc9
AnthonySchuijlenburg <s1114326@student.hsleiden.nl>
2019-12-18 11:29:09 +0100

    Cleaned up experiments into databaseUtilities and PreparedDatabase

--- src/main/java/nl/ipsen3server/dao/PreparedStatmentDatabaseUtilities.java
+++ src/main/java/nl/ipsen3server/dao/PreparedStatmentDatabaseUtilities.java
@@ -3,10 +3,133 @@ package nl.ipsen3server.dao;
 import nl.ipsen3server.models.DatabaseModel;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
 public class PreparedStatmentDatabaseUtilities {
+
+    /**
+     * Call this method to execute a query. This method accepts a query type and calls for the desired method.
+     *
+     * @author AnthonySchuijlenburg
+     *
+     * @param databaseModel DatabaseModel
+     * @param query STRING query ("SELECT * FROM tablename")
+     * @param queryType string SELECT, UPDATE, INSERT, DELETE
+     * @param data the data that needs to be filled into the prepared statement
+     * @return returns a String(JSON) a from database
+     */
+    public String connectToDatabase(DatabaseModel databaseModel, String query, String queryType, ArrayList<String> data){
+
+        String result = null;
+        List<String> updateQueries = new ArrayList<>(Arrays.asList("INSERT", "UPDATE", "DELETE"));
+
+        try {
+            if(queryType.equals("SELECT")) {
+                result = executeQuery(databaseModel, query, data);
+            } else if (updateQueries.contains(queryType)){
+                executeUpdate(databaseModel, query, data);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+
+    /**
+     * Executes a SELECT statement and returns the result in a String format
+     *
+     * @author AnthonySchuijlenburg
+     *
+     * @param databaseModel DatabaseModel
+     * @param query STRING query ("SELECT * FROM tablename")
+     * @param data the data that needs to be filled into the prepared statement, needs to be all strings!
+     * @return String result of entered query
+     */
+    private String executeQuery(DatabaseModel databaseModel, String query, ArrayList<String> data){
+        String username = databaseModel.getUsername();
+        String password = databaseModel.getPassword();
+        int portNumber = databaseModel.getPortNumber();
+        String databaseName = databaseModel.getDatabaseName();
+        String hostName = databaseModel.getHostName();
+
+        String url = createUrl(portNumber, databaseName, hostName);
+        String resultsInJson = null;
+
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+            for(int i = 0; i < data.size(); i++){
+                if (isNumeric(data.get(i))) {
+                    preparedStatement.setInt(i + 1, Integer.parseInt(data.get(i)));
+                } else {
+                    preparedStatement.setString(i + 1, data.get(i));
+                }
+            }
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            JsonConverterUtilities jsonConverter = new JsonConverterUtilities();
+            resultsInJson = jsonConverter.convertToJSON(resultSet).toString();
+
+        } catch (SQLException e) {
+            System.out.println(query);
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println(query);
+            e.printStackTrace();
+        }
+        return resultsInJson;
+    }
+
+
+    /**
+     * Executes and update request (DELETE, UPDATE, INSERT), inserts it into the database
+     *
+     * @author AnthonySchuijlenburg
+     *
+     * @param databaseModel DatabaseModel
+     * @param query STRING query ("SELECT * FROM tablename")
+     * @param data the data that needs to be filled into the prepared statement, needs to be all strings!
+     * @return Nothing
+     */
+    private void executeUpdate(DatabaseModel databaseModel, String query, ArrayList<String> data){
+        String username = databaseModel.getUsername();
+        String password = databaseModel.getPassword();
+        int portNumber = databaseModel.getPortNumber();
+        String databaseName = databaseModel.getDatabaseName();
+        String hostName = databaseModel.getHostName();
+
+        String url = createUrl(portNumber, databaseName, hostName);
+
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+            for(int i = 0; i < data.size(); i++){
+                if (isNumeric(data.get(i))) {
+                    preparedStatement.setInt(i + 1, Integer.parseInt(data.get(i)));
+                } else {
+                    preparedStatement.setString(i + 1, data.get(i));
+                }
+            }
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println(query);
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println(query);
+            e.printStackTrace();
+        }
+    }
+
+    private String createUrl(int portNumber, String databaseName, String hostName) {
+        return String.format("jdbc:postgresql://%s:%s/%s", hostName, portNumber, databaseName);
+    }
 
 
     /**
